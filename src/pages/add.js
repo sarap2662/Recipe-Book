e0d3b404f796eca80195429318fee80174ea6824
sarap2662 <sarapena7@gmail.com>
2024-05-30 20:14:03 -0700

    updated add page

--- src/pages/add.js
+++ src/pages/add.js
@@ -4,6 +4,7 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
+//import image from "../spices.jpg";
 // import axios from "axios";
 
 function Add() {
@@ -46,46 +47,50 @@ function Add() {
 
   return (
     <>
-      <div>
-        <h1 className="intro">Add your favorite Recipes!</h1>
-      </div>
-      <Form onSubmit={handleSubmit}>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formBasicName">
-            <Form.Label htmlFor="name">Recipe Name</Form.Label>
-            <Form.Control type="text" />
-          </Form.Group>
-          <Form.Group as={Col} controlId="formBasicPicture">
-            <Form.Label htmlFor="picture">Picture</Form.Label>
-            <Form.Control type="url" placeholder="Enter picture URL" />
-          </Form.Group>
-        </Row>
-        <Form.Group className="mb-3" controlId="formBasicIngredients">
-          <Form.Label htmlFor="ingredients">Ingredients</Form.Label>
-          {ingredients.map((ingredient, index) => (
-            <div key={index}>
-              <Form.Control
-                type="text"
-                value={ingredient}
-                onChange={(event) => handleAddIngredient(index, event)}
-              />
-              <Button onClick={handleAddClick}>+</Button>
-              {ingredients.length > 1 && (
-                <Button onClick={() => handleRemoveClick(index)}>-</Button>
-              )}
-            </div>
-          ))}
-        </Form.Group>
-        <div className="button-container">
-          <Button
-            className="submit"
-            as="input"
-            type="submit"
-            value="Add Recipe"
-            size="lg"
-          />{" "}
+      <div className="fullPage">
+        <div>
+          <h1 className="intro">Add your favorite Recipes!</h1>
         </div>
-      </Form>
+        <div className="formContainer">
+          <Form onSubmit={handleSubmit}>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formBasicName">
+                <Form.Label htmlFor="name">Recipe Name</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group as={Col} controlId="formBasicPicture">
+                <Form.Label htmlFor="picture">Picture</Form.Label>
+                <Form.Control type="url" placeholder="Enter picture URL" />
+              </Form.Group>
+            </Row>
+            <Form.Group className="mb-3" controlId="formBasicIngredients">
+              <Form.Label htmlFor="ingredients">Ingredients</Form.Label>
+              {ingredients.map((ingredient, index) => (
+                <div key={index}>
+                  <Form.Control
+                    type="text"
+                    value={ingredient}
+                    onChange={(event) => handleAddIngredient(index, event)}
+                  />
+                  <Button onClick={handleAddClick}>+</Button>
+                  {ingredients.length > 1 && (
+                    <Button onClick={() => handleRemoveClick(index)}>-</Button>
+                  )}
+                </div>
+              ))}
+            </Form.Group>
+            <div className="button-container">
+              <Button
+                className="submit"
+                as="input"
+                type="submit"
+                value="Add Recipe"
+                size="lg"
+              />{" "}
+            </div>
+          </Form>
+        </div>
+      </div>
     </>
   );
 }
