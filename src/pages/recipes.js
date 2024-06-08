63490aa9d670f923928ceb0af29366894b4fd3e9
Rasheen Kirkland <kirkland.rasheen@gmail.com>
2024-06-08 07:21:45 -0700

    new branch updated with pending code

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,43 +1,38 @@
 import React from "react";
 import "./pages.css";
-import Buttton from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import { useNavigate } from "react-router-dom";
 
-const RecipeBook = (data) => {
-  //   let recipesFormatted = data.recipes.map((recipe) => {
-  //     return (
-  //       <Col key={recipe.name}>
-  //         <Card style={{ width: "20rem" }}>
-  //           <Card.Img variant="top" src={recipe.picture} />
-  //           <Card.Body>
-  //             <Card.Title>{recipe.name}</Card.Title>
-  //             <Buttton variant="primary">View Recipe</Buttton>
-  //           </Card.Body>
-  //         </Card>
-  //       </Col>
-  //     );
-  //   });
-  // };
+function RecipeBook(data) {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="fullPage">
         <div>
           <h1 className="intro">Turn the pages of your Recipe Book!</h1>
         </div>
+
         <div className="recipeContainer">
           <Row xs={1} md={2} className="g-4">
-            {Array.from({ length: 4 }).map((_, idx) => (
+            {data.recipes.map((recipe, idx) => (
               <Col key={idx}>
                 <Card style={{ width: "20rem" }}>
                   <Card.Img
                     variant="top"
-                    src="https://via.placeholder.com/150x122"
+                    src={recipe.image || "https://via.placeholder.com/150x122"}
                   />
                   <Card.Body>
-                    <Card.Title>Recipe Name</Card.Title>
-                    <Buttton variant="primary">View Recipe</Buttton>
+                    <Card.Title>{recipe.name}</Card.Title>
+                    <Button
+                      variant="primary"
+                      onClick={() => navigate.push(`/recipe/${recipe.id}`)}
+                    >
+                      View Recipe
+                    </Button>
                   </Card.Body>
                 </Card>
               </Col>
@@ -47,6 +42,6 @@ const RecipeBook = (data) => {
       </div>
     </>
   );
-};
+}
 
 export default RecipeBook;
